beda232639da14dded9eb0fdb17fd2026f37348a
william <williampius17@gmail.com>
2021-01-31 22:25:22 +0300

    edited user models

--- src/models/users.model.js
+++ src/models/users.model.js
@@ -1,40 +1,116 @@
 // See http://docs.sequelizejs.com/en/latest/docs/models-definition/
 // for more of what you can do here.
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 const DataTypes = Sequelize.DataTypes;
 
-module.exports = function (app) {
-  const sequelizeClient = app.get('sequelizeClient');
-  const users = sequelizeClient.define('users', {
-  
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-  
-  
-    googleId: { type: DataTypes.STRING },
-  
-    facebookId: { type: DataTypes.STRING },
-  
-  }, {
-    hooks: {
-      beforeCount(options) {
-        options.raw = true;
-      }
-    }
-  });
+module.exports = function(app) {
+    const sequelizeClient = app.get("sequelizeClient");
+    const users = sequelizeClient.define(
+        "users", {
+            email: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                unique: true,
+            },
+            password: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            firstname: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            lastname: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            phone: {
+                type: DataTypes.NUMBER,
+                allowNull: false,
+            },
+            location: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            idnumber: {
+                type: DataTypes.NUMBER,
+                allowNull: false,
+            },
+            street: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            location: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            city: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            about: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            styles: {
+                type: DataTypes.ARRAY,
+                allowNull: false,
+            },
+            role: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            info: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            newsletter: {
+                type: DataTypes.BOOLEAN,
+                allowNull: false,
 
-  // eslint-disable-next-line no-unused-vars
-  users.associate = function (models) {
-    // Define associations here
-    // See http://docs.sequelizejs.com/en/latest/docs/associations/
-  };
+            },
+            //verification
+            isVerified: {
+                type: DataTypes.BOOLEAN,
+                allowNull: false,
+            },
+            verifyToken: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            resetToken: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            verifyExpires: {
+                type: DataTypes.DATE,
+                allowNull: false,
+            },
+            verifyChanges: {
+                type: DataTypes.JSON,
+                allowNull: false,
+            },
+            resetExpires: {
+                type: DataTypes.DATE,
+                allowNull: false,
+            },
+            googleId: { type: DataTypes.STRING },
 
-  return users;
+            facebookId: { type: DataTypes.STRING },
+        }, {
+            hooks: {
+                beforeCount(options) {
+                    options.raw = true;
+                },
+            },
+        }
+    );
+
+    // eslint-disable-next-line no-unused-vars
+    users.associate = function(models) {
+        // Define associations here
+        // See http://docs.sequelizejs.com/en/latest/docs/associations/
+    };
+
+    return users;
 };
