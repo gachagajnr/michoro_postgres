22a52dfc1a997843c07d2d0805bf2bed328c1819
william <williampius17@gmail.com>
2021-02-01 21:27:59 +0300

    initialize mails service to store sent messages

--- src/services/index.js
+++ src/services/index.js
@@ -2,10 +2,12 @@ const users = require('./users/users.service.js');
 const arts = require('./arts/arts.service.js');
 const authmanagement = require('./authmanagement/authmanagement.service.js');
 const mailer = require('./mailer/mailer.service.js');
+const mails = require('./mails/mails.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(users);
   app.configure(arts);
   app.configure(authmanagement);
   app.configure(mailer);
+  app.configure(mails);
 };
---
+++ src/services/mails/mails.class.js
@@ -0,0 +1,5 @@
+const { Service } = require('feathers-sequelize');
+
+exports.Mails = class Mails extends Service {
+  
+};
