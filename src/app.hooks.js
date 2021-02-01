8878ae926156a291285e79702a6ee50c8df0edc9
william <williampius17@gmail.com>
2021-02-01 20:37:11 +0300

    app hooks updated

--- src/app.hooks.js
+++ src/app.hooks.js
@@ -1,33 +1,60 @@
 // Application hooks that run for every service
+const { when } = require("feathers-hooks-common");
+const authorize = require("./hooks/abilities");
+const authenticate = require("./hooks/authenticate");
 
 module.exports = {
-  before: {
-    all: [],
-    find: [],
-    get: [],
-    create: [],
-    update: [],
-    patch: [],
-    remove: []
-  },
+    before: {
+        all: [],
+        find: [],
+        get: [],
+        create: [
+            when(
+                (hook) => hook.params.provider && `/${hook.path}` !== "/authentication",
+                authenticate,
+                authorize()
+            ),
+        ],
+        update: [
+            when(
+                (hook) => hook.params.provider && `/${hook.path}` !== "/authentication",
+                authenticate,
+                authorize()
+            ),
+        ],
+        patch: [
+            when(
+                (hook) => hook.params.provider && `/${hook.path}` !== "/authentication",
+                authenticate,
+                authorize()
+            ),
+        ],
+        remove: [
+            when(
+                (hook) => hook.params.provider && `/${hook.path}` !== "/authentication",
+                authenticate,
+                authorize()
+            ),
+        ],
+    },
 
-  after: {
-    all: [],
-    find: [],
-    get: [],
-    create: [],
-    update: [],
-    patch: [],
-    remove: []
-  },
+    after: {
+        all: [],
+        find: [],
+        get: [],
+        create: [],
+        update: [],
+        patch: [],
+        remove: [],
+    },
 
-  error: {
-    all: [],
-    find: [],
-    get: [],
-    create: [],
-    update: [],
-    patch: [],
-    remove: []
-  }
+    error: {
+        all: [],
+        find: [],
+        get: [],
+        create: [],
+        update: [],
+        patch: [],
+        remove: [],
+    },
 };
