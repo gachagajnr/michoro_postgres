bd0668175efd03903cede35f011b70cc0d95552e
william <williampius17@gmail.com>
2021-02-01 20:31:05 +0300

    hooks updated with code

--- src/hooks/authenticate.js
+++ src/hooks/authenticate.js
@@ -1,9 +1,22 @@
-// Use this hook to manipulate incoming or outgoing data.
-// For more information on hooks see: http://docs.feathersjs.com/api/hooks.html
+const { authenticate } = require("@feathersjs/authentication").hooks;
+const { NotAuthenticated } = require("@feathersjs/errors");
+const verifyIdentity = authenticate("jwt");
 
-// eslint-disable-next-line no-unused-vars
-module.exports = (options = {}) => {
-  return async context => {
-    return context;
-  };
+function hasToken(hook) {
+    if (hook.params.headers == undefined) return false;
+    if (hook.data.accessToken == undefined) return false;
+    return hook.params.headers.authorization || hook.data.accessToken;
+}
+
+module.exports = async function authenticate(hook) {
+    try {
+        // console.log(hook);
+        return await verifyIdentity(hook);
+    } catch (error) {
+        if (error instanceof NotAuthenticated && !hasToken(hook)) {
+            return hook;
+        }
+
+        throw error;
+    }
 };
--- src/hooks/upload-art.js
+++ src/hooks/upload-art.js
@@ -3,7 +3,39 @@
 
 // eslint-disable-next-line no-unused-vars
 module.exports = (options = {}) => {
-  return async context => {
-    return context;
-  };
+    return async(context) => {
+        const { app, data, params } = context;
+
+        if (!data) {
+            throw new Error("no data");
+        }
+        await app
+            .service("uploads")
+            .create({ uri: data.uri })
+            .then((res) => {
+                context.data = {
+                    name: data.name,
+                    price: data.price,
+                    description: data.description,
+                    orientation: data.orientation,
+                    author: context.params.user,
+                    length: data.length,
+                    width: data.width,
+                    category: data.category,
+                    breadth: data.breadth,
+                    materials: data.materials,
+                    keywords: data.keywords,
+                    mediums: data.mediums,
+                    styles: data.styles,
+                    subjects: data.subjects,
+
+                    art: res.id,
+                };
+            })
+            .catch((e) => {
+                throw new Error("Not successful");
+            });
+
+        return context;
+    };
 };
