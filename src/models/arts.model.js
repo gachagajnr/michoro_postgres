30e4c1b817430135d191ed9817d29e6c9a3a33df
william <williampius17@gmail.com>
2021-03-10 23:16:59 +0300

    new public files

--- src/models/arts.model.js
+++ src/models/arts.model.js
@@ -6,93 +6,99 @@ const DataTypes = Sequelize.DataTypes;
 module.exports = function(app) {
     const sequelizeClient = app.get("sequelizeClient");
     const arts = sequelizeClient.define(
-        "arts", {
-            _id: {
-                type: Sequelize.UUID,
-                unique: true,
-                primaryKey: true,
-                isUUID: 4,
-                defaultValue: Sequelize.UUIDV4
-              },
-            name: {
-                type: DataTypes.TEXT,
-                // allowNull: false,
-            },
-            price: {
-                type: DataTypes.TEXT,
-                // allowNull: false,
-            },
-            description: {
-                type: DataTypes.STRING,
-                // allowNull: false,
-            },
-            length: {
-                type: DataTypes.STRING,
-                // allowNull: false,
-            },
-            width: {
-                type: DataTypes.STRING,
-                // allowNull: false,
-            },
-            breadth: {
-                type: DataTypes.STRING,
-                // allowNull: false,
-            },
-            mediums: {
-                type: DataTypes.ARRAY(Sequelize.STRING),
-                // allowNull: false,
-            },
-            keywords: {
-                type: DataTypes.ARRAY(Sequelize.STRING),
-                // allowNull: false,
-            },
-            category: {
-                type: DataTypes.STRING,
-                // allowNull: false,
-            },
-            orientation: {
-                type: DataTypes.STRING,
-                // allowNull: false,
-            },
-            subjects: {
-                type: DataTypes.ARRAY(Sequelize.STRING),
-                // allowNull: false,
-            },
-            styles: {
-                type: DataTypes.ARRAY(Sequelize.STRING),
-                // allowNull: false,
-            },
-            materials: {
-                type: DataTypes.ARRAY(Sequelize.STRING),
-                // allowNull: false,
-            },
-            visibleToPublic: {
-                type: DataTypes.BOOLEAN,
-                defaultValue: true,
-            },
-            art: {
-                type: DataTypes.STRING,
-                // allowNull: false,
-            },
-            url: {
-                type: DataTypes.STRING,
-                // allowNull: false,
-            },
-            isSold: {
-                type: DataTypes.BOOLEAN,
-                defaultValue: false,
-            },
-            soldBy: {
-                type: DataTypes.STRING,
-                defaultValue: 'Michoro',
-            },
-        }, {
-            hooks: {
-                beforeCount(options) {
-                    options.raw = true;
-                },
-            },
-        }
+      "arts",
+      {
+        _id: {
+          type: Sequelize.UUID,
+          unique: true,
+          primaryKey: true,
+          isUUID: 4,
+          defaultValue: Sequelize.UUIDV4,
+        },
+        name: {
+          type: DataTypes.TEXT,
+          // allowNull: false,
+        },
+        price: {
+          type: DataTypes.TEXT,
+          // allowNull: false,
+        },
+        description: {
+          type: DataTypes.STRING,
+          // allowNull: false,
+        },
+        length: {
+          type: DataTypes.STRING,
+          // allowNull: false,
+        },
+        size: {
+          type: DataTypes.STRING,
+          // allowNull: false,
+        },
+        width: {
+          type: DataTypes.STRING,
+          // allowNull: false,
+        },
+        breadth: {
+          type: DataTypes.STRING,
+          // allowNull: false,
+        },
+        mediums: {
+          type: DataTypes.ARRAY(Sequelize.STRING),
+          // allowNull: false,
+        },
+        keywords: {
+          type: DataTypes.ARRAY(Sequelize.STRING),
+          // allowNull: false,
+        },
+        category: {
+          type: DataTypes.STRING,
+          // allowNull: false,
+        },
+        orientation: {
+          type: DataTypes.STRING,
+          // allowNull: false,
+        },
+        subjects: {
+          type: DataTypes.ARRAY(Sequelize.STRING),
+          // allowNull: false,
+        },
+        styles: {
+          type: DataTypes.ARRAY(Sequelize.STRING),
+          // allowNull: false,
+        },
+        materials: {
+          type: DataTypes.ARRAY(Sequelize.STRING),
+          // allowNull: false,
+        },
+        visibleToPublic: {
+          type: DataTypes.BOOLEAN,
+          defaultValue: true,
+        },
+        art: {
+          type: DataTypes.STRING,
+          // allowNull: false,
+        },
+        url: {
+          type: DataTypes.STRING,
+          // allowNull: false,
+        },
+        isSold: {
+          type: DataTypes.BOOLEAN,
+          defaultValue: false,
+        },
+        soldBy: {
+          type: DataTypes.STRING,
+          defaultValue: "Michoro",
+        },
+      },
+      {
+        hooks: {
+          beforeCount(options) {
+            options.raw = true;
+          },
+        },
+      }
     );
 
     // eslint-disable-next-line no-unused-vars
